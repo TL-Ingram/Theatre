5d856bae00612f9eca2ac0de6f3e92edef0ff7c0
Ingram Thomas <Thomas.Ingram@wwl.nhs.uk>
2022-06-27 11:11:59 +0100

    last push incorrect. This is correct

--- _sql_hits.R
+++ _sql_hits.R
@@ -19,14 +19,20 @@ process_one <- data_theatre %>%
   filter(surgery_date_dt > "2022-01-01" & surgery_date_dt < "2022-06-17") %>%
   filter(site_description == "Wrightington Hospital") %>%
   filter(operation_usage == "Operation Completed") %>%
-  select(c(1,2,5,15,18,27,28,30,32,34,35,36,37,38,39,40,42,43,44,51,52,53,55,56,57,
-           58,59,83,84,85,86,88,89,90,91,92,93,108,109,128,129,130,131,132,
-           133,137,138,139,140,141,142,143,150:164))
+  select(c(1,2,5,15,18,27,28,30,32,34,35,36,37,38,39,40,42,43,44,51,52,53,55,
+           56,57,58,59,83,84,85,86,88,89,90,91,92,93,108,109,128,129,130,131,
+           132,133,137,138,139,140,141,142,143,150:164))
+
 df_dt_only <- process_one %>%
   select(c(1,6:19,46:51,53,64,66:67)) %>%
   select(c(1:15,22,24,25)) %>%
   select(c(1,11,9,2:8,10,12:18)) %>%
-  select(c(1:8,14,9,12,10:11,13,15:18)) %>%
+  select(c(1,4:8,14,9,12,10:11,13,15,17,18)) %>%
+  mutate(check = if_else(sent_for == porter_time, TRUE, FALSE)) %>%
+  mutate(across(c(2:13), hm)) %>%
+  mutate_at(vars(c(2:13)), function(x) hour(x)*60 + minute(x)) %>%
+  
 # check if columns are actually duplicated 
+# removed either sent_for or porter_time. Too many NAs. Therefore saved only whichever time was earlier.
 # time difference in between each stage
 # actual operation time may not matter, although could group by type?
